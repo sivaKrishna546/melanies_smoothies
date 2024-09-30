2f1daa6300cdaa0aa6930e7aaefbf67f306ce6b2
sivaKrishna546 <sivaonlinealert@gmail.com>
2024-09-30 18:50:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,57 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """ Choose the fruits you want in your custom Smoothie!
+    """
+)
+
+#option=st.selectbox(
+#    'How would you like to be contacted?',
+#    ('Email', 'Home Phone', 'Mobile Phone')
+#   )
+#st.write('You selected:', option)
+
+#option=st.selectbox(
+#    'What is your favorite fruit?',
+#    ('Banana', 'Strawberries', 'Peaches', 'Apple', 'Grapes', 'Mango')
+#)
+#st.write('Your favorite fruit is:', option)
+#title=st.text_input('Movie Title','Life of Brian')
+#st.write('The Current Movie Title is', title)
+
+name_on_order=st.text_input('Name on Smoothie:')
+st.write('The Name on your Smoothie will be:', name_on_order)
+
+#session=get_active_session()
+cnx=st.connection('snowflake')
+session=cnx.session()
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe,use_container_width=True)
+ingredients_list=st.multiselect('Choose up to 5 ingredients:'
+                                , my_dataframe
+                                , max_selections=5)
+if ingredients_list:
+    ingredients_string=''
+    for fruit_choosen in ingredients_list:
+        ingredients_string+=fruit_choosen+' '
+    #st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string +"""','"""+name_on_order+"""')"""
+    #st.write(my_insert_stmt)
+    #st.stop()
+    time_to_insert=st.button('Submit Order')
+    #if ingredients_string:
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered,'+ name_on_order+'!', icon="✅")
+        
+
+# Import python packages
+import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
